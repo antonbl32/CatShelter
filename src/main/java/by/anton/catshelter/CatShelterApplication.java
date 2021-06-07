271fbbf2c9e24807e5e325b99a8d918f22146778
antonbl32 <anton2.bl32@gmail.com>
2021-06-07 18:39:05 +0300

    first commit

---
+++ src/main/java/by/anton/catshelter/CatShelterApplication.java
@@ -0,0 +1,13 @@
+package by.anton.catshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatShelterApplication.class, args);
+    }
+
+}
